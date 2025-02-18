83e5c7ae3ef8bd232ac485b95aaa2576f3255f5f
ChrisShep98 <shepardchristopher1@gmail.com>
2025-02-18 16:36:18 -0500

    apollo server setup and added RegisterResolver

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { buildSchema } from "type-graphql";
+import { RegisterResolver } from "../../../../graphql/resolvers/register";
+
+const schema = await buildSchema({
+  resolvers: [RegisterResolver],
+  validate: false,
+});
+
+const server = new ApolloServer({ schema });
+
+export type MyContext = {
+  req: NextApiRequest;
+  res: NextApiResponse;
+};
+
+const handler = startServerAndCreateNextHandler(server, {
+  context: async (req, res): Promise<MyContext> => ({
+    req,
+    res,
+  }),
+});
+
+export { handler as GET, handler as POST }; // works with Next.js API routes
