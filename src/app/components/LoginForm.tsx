911b13196e540af0edadb5a980e2977763e9b6ac
ChrisShep98 <shepardchristopher1@gmail.com>
2025-02-26 16:28:08 -0500

    removed material ui components, going to try out shadcn for this

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -1,18 +1,19 @@
-import { Box, Button, Stack } from "@mui/material";
 import { signIn } from "@/app/auth";
 import Image from "next/image";
 import GoogleIcon from "images/google-icon.svg";
 
 export default function LoginForm() {
   return (
-    <Box
-      boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"}
-      position={"absolute"}
-      p={6}
-      borderRadius={"30px"}
-      sx={{ backgroundColor: "white" }}
+    <div
+      style={{
+        boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+        position: "absolute",
+        padding: 24,
+        borderRadius: "30px",
+        backgroundColor: "white",
+      }}
     >
-      <Stack direction={"row"} alignItems={"center"}>
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
         <Image src={GoogleIcon} alt="Google Icon" width={50} height={50} />
         <form
           action={async () => {
@@ -20,9 +21,9 @@ export default function LoginForm() {
             await signIn("google", { redirectTo: "/dashboard" });
           }}
         >
-          <Button type="submit">Signin with Google</Button>
+          <button type="submit">Signin with Google</button>
         </form>
-      </Stack>
-    </Box>
+      </div>
+    </div>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,33 @@
 import { auth, signOut } from "@/app/auth";
-import { Button } from "@mui/material";
 import Image from "next/image";
 
 export default async function Dashboard() {
   const session = await auth();
 
   const sessionDate = {
-    name: session.user.name,
-    picture: session.user.image,
+    name: session?.user?.name,
+    picture: session?.user?.image,
   };
 
   return (
     <div>
       <h1>Hello {sessionDate.name}!</h1>
       <Image
-        src={sessionDate.picture}
+        src={sessionDate.picture!}
         alt="Google picture"
         height={96}
         width={96}
         quality={100}
         style={{ borderRadius: "50%" }}
       />
-      <Button
+      <button
         onClick={async () => {
           "use server";
           await signOut({ redirectTo: "/" });
         }}
       >
         Sign out
-      </Button>
+      </button>
     </div>
   );
 }
